f64c77c39070661b90db02f917ac8e28961752e5
rsrinath158818 <2195$ranj>
2019-10-16 11:29:48 +0530

    Assignment1

--- Assignment1.js
+++ Assignment1.js
@@ -1,37 +1,54 @@
 var fs = require('fs');
-const readline = require('readline');
-const rl = readline.createInterface({
+const arg = require('yargs').argv;
+var numberOfArgs = process.argv.length;
+
+if(numberOfArgs > 2) {
+
+    for(var i=2; i< numberOfArgs; i++)
+    {
+        var fn = process.argv[i]+ " ";
+       fileUpdate(fn.toString());
+    }
+    return;
+} else {
+    const readline = require('readline');
+    const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
-  var fileToBeAdded;
   rl.question('Please enter the file name : ', (answer) => {
-      if(answer){
-        fileToBeAdded = answer.toString();
+      if(answer){   
+       fileUpdate(answer.toString());
+      } else {
+        console.log('Exiting program');
+      }
+    rl.close();
+  });
 
+}
+
+  function fileUpdate (answer) 
+   {
+        var fileToBeAdded = answer.toString();
         if(fs.readFileSync('./filenames.txt').toString().includes(fileToBeAdded.toString())) {
-            console.log('File already exists !! Choose a different file name ');
+            console.log('File '+fileToBeAdded.toString()+'already exists !! Choose a different file name ');
         }
 
         else {
             fs.appendFile('./filenames.txt' , '\n'+fileToBeAdded.toString(),function(err){
                 if(err) {
                     return console.error(err);
-                } else {
-                    console.log('Creating file...');
                 }
             });
-           
+        
             fs.writeFile(fileToBeAdded.toString() , 'You are awesome!!!!',function(err){
                 if(err) {
                     return console.error(err);
                 } else {
-                    console.log('File created successfully ');
+                    console.log('File created successfully : ', fileToBeAdded.toString());
                 }
             });
-        }
-       
-      }
-    rl.close();
-  });
+          }
+
+    }
  
